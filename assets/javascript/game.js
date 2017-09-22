110e8bbf0b2a1713c42d1e2b950b788ff400e273
Winfred Sunga <winfredsunga01@gmail.com>
2017-09-21 22:25:28 -0700

    completed display of letters guessed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,3 @@
-//Pseudocode :
-//variables for - score, current word to guess, words to guess,  guesses remaining, letters guessed, valid letters (alphabet only, exclude any other keys)
 var hangMan = {
   serengetiAnimals : ["porcupine","hedgehog","badger","hippopotamus","gazelle","rhinoceros","waterbuck","wildebeest","flamingo","antelope","hornbill","chameleon","babboon","buffalo","lion","monkey","tiger", "cheetah", "impala"],
   wins : 0,
@@ -8,7 +6,7 @@ var hangMan = {
   wordsAlreadyUsed : [],
   currentWordString : "",
   currentWordLetters : [], //this.currentWordString.split(''),
-  invisibleLetters : [], // will have '_' to replace letters
+  invisibleLetters : "", // will have '_' to replace letters
   displayWord : [], //this.invisibleLetters.split('').join(' '),
   remainingGuesses : 12,
   alphabet : 'abcdefghijklmnopqrstuvwxyz'.split(''),
@@ -22,6 +20,12 @@ var hangMan = {
   addWordToUsedList : function(word) {
     this.wordsAlreadyUsed.push(word);
   },
+  updateCurrentWordLetters : function() {
+    this.currentWordLetters = this.currentWordString.split('');
+  },
+  updateInvisibleLetters : function(word) {
+    this.invisibleLetters = word;
+  },
   hideLetters : function(word) {
     var invisibleWord = "";
 
@@ -29,11 +33,19 @@ var hangMan = {
       invisibleWord += "_";
     }
     
-    this.updateDisplayWord(invisibleWord);
-    return invisibleWord;
+    //update invisible letters
+    this.updateInvisibleLetters(invisibleWord);
+
+    //update display word
+    this.updateDisplayWord();
+
+    //return invisibleWord;
   },
-  updateDisplayWord : function(word) {
-    this.displayWord = word.split('').join(' ');
+  convertCurrentWordIntoArray : function(word) {
+    this.invisibleLetters = word;
+  },
+  updateDisplayWord : function() {
+    this.displayWord = this.invisibleLetters.split('');
   },
   pickRandomWord : function() {
     var randomWord = this.serengetiAnimals[Math.floor(Math.random() * this.serengetiAnimals.length)];
@@ -43,30 +55,64 @@ var hangMan = {
         this.currentWordString = randomWord;
       } else if(this.wordsAlreadyUsed.length < this.serengetiAnimals.length && this.wordsAlreadyUsed.indexOf(randomWord) === -1) {
         this.currentWordString = randomWord;  
+      } else if (this.wordsAlreadyUsed.length === this.serengetiAnimals.length){
+        alert("You ran out of words to guess. Game Over!");
+        break;
       } else { //(this.wordsAlreadyUsed.length < this.serengetiAnimals.length && wordsAlreadyUsed.indexOf(randomWord) === 1) {
-        randomWord = Math.floor(Math.random() * this.serengetiAnimals.length);
+        randomWord = this.serengetiAnimals[Math.floor(Math.random() * this.serengetiAnimals.length)];
         continue;
       }
     }
     
+    //convert the current word into an array
+    this.convertCurrentWordIntoArray(randomWord);
+
+    //update current word letters
+    this.updateCurrentWordLetters();
+
     //each time a new word gets picked, add that word into wordsAlreadyUsed 
     this.addWordToUsedList(randomWord);
 
-    //replace the individual letters with "_" and display on the game console
-    this.displayWordToGuess(this.hideLetters(randomWord));
+    //hide the letters with "_"
+    this.hideLetters(randomWord);
+
+    // display word on the game console
+    this.displayWordToGuess();
+
 
 
   },
-  displayWordToGuess : function(word) {
-    this.invisibleLetters = word;
-    document.getElementById("current_word").innerHTML = this.displayWord;
+  displayWordToGuess : function() {
+    
+    document.getElementById("current_word").innerHTML = this.displayWord.join(' ');
   },
+  displayGuessedLetters : function() {
+    document.getElementById("letters_guessed").innerHTML = this.lettersGuessed.join(' , ');
+  },
+  unhideLetter : function(letter) {
+    for(var i = 0; i < this.currentWordLetters.length; i++) {
+      if(letter === this.currentWordLetters[i]) {
+        this.displayWord[i] = letter;
+        this.invisibleLetters = this.displayWord.join('');
+        console.log("letter : " + letter);
+        console.log("display word's type is : " + typeof(this.displayWord));
+        console.log('current word letters : ' + this.currentWordLetters);
+        console.log("invisible letter : " + this.invisibleLetters);
+        console.log("display word : " + this.displayWord);
+        console.log("does it even go in here?");
+      }
+    }
+    //updateInvisibleLetters();
+    this.updateDisplayWord();
+  }
+  ,
   displayRemainingGuesses : function() {
     document.getElementById("remaining_guesses").innerHTML = this.remainingGuesses;
   },
   decrementRemainingGuesses : function() {
     this.remainingGuesses--; // = this.remainingGuesses - 1;
   }
+
 };
 
 hangMan.displayWins();
@@ -74,14 +120,65 @@ hangMan.displayLoses();
 hangMan.pickRandomWord();
 //hangMan.displayWordToGuess();
 hangMan.displayRemainingGuesses();
-hangMan.decrementRemainingGuesses();
+//hangMan.decrementRemainingGuesses();
 
 //hangMan.listWordsAvailableForGuessing();
 console.log(hangMan.currentWordString);
-console.log('random # : ' + Math.floor(Math.random() * hangMan.serengetiAnimals.length));
 console.log('word already used : ' + hangMan.wordsAlreadyUsed);
-console.log('invisible word : ' + hangMan.invisibleWord);
+console.log('invisible letters : ' + hangMan.invisibleLetters);
 console.log('remaining guesses : ' + hangMan.remainingGuesses);
+
+document.onkeyup = function(event) {
+  var playerInput = event.key.toLowerCase();
+
+  if(hangMan.alphabet.indexOf(playerInput) === -1) {
+    alert("You pressed an invalid key! Please choose another letter...");
+  } else if(hangMan.lettersGuessed.indexOf(playerInput) !== -1) {
+    alert("You have already guessed letter " + playerInput + '. Please pick another letter!')
+  } else if(hangMan.lettersGuessed.indexOf(playerInput) === -1 && hangMan.currentWordString.indexOf(playerInput) !== -1){
+    hangMan.unhideLetter(playerInput);
+    hangMan.displayWordToGuess();
+    hangMan.lettersGuessed.push(playerInput);
+    hangMan.displayGuessedLetters();
+    hangMan.decrementRemainingGuesses();
+    hangMan.displayRemainingGuesses();
+    console.log("remaining guesses: " + hangMan.remainingGuesses)
+    console.log("letters guessed : " + hangMan.lettersGuessed);
+    console.log("invisible letters " + hangMan.invisibleLetters);
+    console.log("display word " + hangMan.displayWord);
+    hangMan.displayWordToGuess();
+  } else if(hangMan.lettersGuessed.indexOf(playerInput) === -1 && hangMan.currentWordString.indexOf(playerInput) === -1) {
+    hangMan.lettersGuessed.push(playerInput);
+    hangMan.displayGuessedLetters();
+    hangMan.decrementRemainingGuesses();
+    hangMan.displayRemainingGuesses();
+    console.log("remaining guesses: " + hangMan.remainingGuesses)
+    //console.log("letters guessed : " + hangMan.lettersGuessed);
+  }
+
+  //check if the player wins or not, and switch to new word
+  if(hangMan.displayWord.indexOf("_") === -1 && hangMan.wordsAlreadyUsed.length !== hangMan.serengetiAnimals.length) {
+    alert("You guessed the word!");
+    hangMan.wins++;
+    hangMan.remainingGuesses = 12;
+    hangMan.pickRandomWord();
+    hangMan.displayWins();
+    hangMan.displayLoses();
+  } else if (hangMan.remainingGuesses === 0 && hangMan.wordsAlreadyUsed.length !== hangMan.serengetiAnimals.length) {
+    alert("You failed to guess the word!");
+    hangMan.loses++;
+    hangMan.remainingGuesses = 12;
+    hangMan.pickRandomWord();
+    hangMan.displayWins();
+    hangMan.displayLoses();
+  }
+
+
+
+}
+
+//Pseudocode :
+//variables for - score, current word to guess, words to guess,  guesses remaining, letters guessed, valid letters (alphabet only, exclude any other keys)
 
 //hangMan.listWordsAvailableForGuessing();
 
@@ -100,10 +197,13 @@ console.log('remaining guesses : ' + hangMan.remainingGuesses);
 
 //capture the letters the player guesses
   //if the key pressed is not a letter of the alphabet 
-    //display error message if the key is not valid
-  //if the letter was already guessed, do not add it into the list of letters guessed
-    //display an error message if the player guessed the same letter more than once
-  // else add the guessed letter into the list 
+    //display an alert
+  //if the letter was already guessed
+    // display an alert
+  // else 
+    //add the guessed letter into the list of letters guessed
+    //if the letter guessed is correct
+      //show the letter in the console
 
 //decrement the guesses remaining each time a letter is guessed
 
